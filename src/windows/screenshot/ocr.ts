e32b8d43020d07c8a4caad606b7acda0b03e9108
幽儿希卡 <yorshka@hotmail.com>
2023-09-28 15:07:53 +0900

    feat: remove local ocr lib

--- src/windows/screenshot/ocr.ts
+++ src/windows/screenshot/ocr.ts
@@ -1,52 +1,4 @@
-import { createWorker } from 'tesseract.js';
-
-const worker = await createWorker({
-  logger: (m) => console.log(m),
-});
-
 type Lang = 'jpn' | 'eng' | 'zh';
-
-export async function init(lang: Lang = 'eng') {
-  await worker.loadLanguage(lang);
-  await worker.initialize(lang);
-}
-
-export async function changeOCRLang(lang: Lang) {
-  await worker.terminate();
-
-  await worker.loadLanguage(lang);
-  await worker.initialize(lang);
-}
-
-export async function ocrText(img: string) {
-  const data = await worker.recognize(img);
-  console.log(data);
-  const {
-    data: { text },
-  } = data;
-  const nText = postProcessText(text);
-
-  return nText;
-}
-
-const replaceMap: Array<[string, string]> = [
-  ['①', '1'],
-  ['②', '2'],
-  ['③', '3'],
-  ['④', '4'],
-  ['⑤', '5'],
-  ['⑥', '6'],
-  ['⑦', '7'],
-  ['⑧', '8'],
-  ['⑨', '9'],
-];
-
-function postProcessText(text: string) {
-  for (const [key, value] of replaceMap) {
-    text = text.replaceAll(key, value);
-  }
-  return text;
-}
 
 const translateLangMap: Record<Lang, string> = {
   jpn: 'ja',
@@ -55,9 +7,7 @@ const translateLangMap: Record<Lang, string> = {
 };
 
 export async function handleOCR(img: string, lang: Lang = 'zh') {
-  // const text = await window.api.ocrTextOnline(img);
-  // console.log('ocrOnline', text);
-  const textContent = await ocrText(img);
+  const textContent = await window.api.ocrTextOnline(img, 'ja');
 
   console.log('textContent');
   console.log(textContent);
